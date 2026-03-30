a4139f97d533929b28ffe84a322da766bb435c8b
4530-bit <jhj50464530@gmail.com>
2026-03-31 08:09:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import re
 import difflib
 
 st.set_page_config(page_title="타운보드 센터 배정용", layout="wide")
-st.title("📍 타운보드 센터 배정용")
+st.title("⭐타운보드 센터 배정용"🚀)
 st.info("""
 **작동 방식**
 - 가동리스트는 자동으로 불러옵니다.
